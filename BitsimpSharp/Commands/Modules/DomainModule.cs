826ad4de687ffab3a69ef00b5b606254e573c94a
Philip <philip+github@tamsweb.de>
2021-04-12 03:49:23 +0200

    added registrant name and organisation
    added registrant name and organisation

--- BitsimpSharp/Commands/Modules/DomainModule.cs
+++ BitsimpSharp/Commands/Modules/DomainModule.cs
@@ -33,8 +33,10 @@ namespace BitsimpBot.Commands.Modules
                     {
                         Title = (domaindata.domain is null || domaindata.domain == "") ? "undefined" : domaindata.domain,
                         Description = (domaindata.domain is null || domaindata.domain == "") ? "undefined" : domaindata.domain,
-                        Timestamp = DateTime.UtcNow
+                        Timestamp = DateTime.UtcNow 
                     };
+                    discordembed.AddField("Registrant Name", (domaindata.registrant.name is null || domaindata.registrant.name == "") ? "undefined" : domaindata.registrant.name);
+                    discordembed.AddField("Registrant Organisation", (domaindata.registrant.organization is null || domaindata.registrant.organization == "") ? "undefined" : domaindata.registrant.organization);
                     discordembed.AddField("Domain Created", (domaindata.createdDate is null || domaindata.createdDate.ToString() == "") ? "undefined" : domaindata.createdDate, true);
                     discordembed.AddField("Domain Updated", (domaindata.updateDate is null || domaindata.updateDate.ToString() == "") ? "undefined" : domaindata.updateDate, true);
                     discordembed.AddField("Domain Status", (domaindata.status is null || domaindata.status == "") ? "undefined" : domaindata.status, false);
